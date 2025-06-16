aa0bf62954c1ed2cad3e9b98eea795d438a0be80
chikuwa-ah <chikuwah21@gmail.com>
2025-06-16 18:36:24 +0900

    sdfakdfak

--- script/typing/main.js
+++ script/typing/main.js
@@ -33,7 +33,7 @@ const arrayShuffle = (array) => {
 };
 
 const createTypingDisplay = () => {
-    const display = document.querySelector('.typing-display');
+    const display = document.querySelector(".typing-display");
     display.innerHTML = `
     <div class="keyboard">
             <div class="key-row">
@@ -90,12 +90,13 @@ const createTypingDisplay = () => {
         </div>
     </div>
     `;
-}
+};
 
 const question = (wordList) => {
     createTypingDisplay();
     let index = 0;
     let next = false;
+    const missList = [];
 
     const meanSpace = document.getElementById("meanText");
     const typeSpace = document.getElementById("typingSpace");
@@ -134,7 +135,7 @@ const question = (wordList) => {
         if (next) {
             index++;
             if (index >= wordList.length) {
-                typingEnd(wordList);
+                typingEnd(wordList, missList);
                 return;
             }
             next = false;
@@ -172,6 +173,7 @@ const question = (wordList) => {
                 showWeak.classList.remove("weak-false");
                 showWeak.classList.add("weak-true");
                 localStorage.setItem(`type-${wordList[index].number}`, true);
+                missList.push(wordList[index]);
                 setTimeout(() => {
                     if (next) {
                         typeSpace.style.color = "#ffffff";
@@ -200,24 +202,48 @@ const question = (wordList) => {
     });
 };
 
-
-const typingEnd = (wordList) => {
+const typingEnd = (wordList, missList) => {
     removeTypingDisplay();
-    createTable(wordList);
+    createTable(wordList, missList);
     wordTouchEvent();
-    const select = document.querySelector('.select-destiny');
-    select.style.display = 'flex';
-}
+    const endPart = document.querySelector(".display");
+    const select = document.querySelector(".select-destiny");
+    endPart.style.display = "block";
+
+    select.addEventListener(
+        "click",
+        (e) => {
+            endPart.style.display = "none";
+            if (e.target.textContent === "設定に戻る") {
+                location.reload();
+                return;
+            } else if (e.target.textContent === "もう一度") {
+                question(wordList);
+                return;
+            } else {
+                if (missList.length > 1) {
+                    question(missList);
+                    return;
+                } else {
+                    location.reload();
+                    return;
+                }
+            }
+            location.reload();
+        },
+        { once: true }
+    );
+};
 
 const removeTypingDisplay = () => {
-    const display = document.querySelector('.typing-display');
-    display.innerHTML = '';
-}
+    const display = document.querySelector(".typing-display");
+    display.innerHTML = "";
+};
 
-const createTable = (data) => {
-    const parent = document.getElementById('table-container');
+const createTable = (data, missList) => {
+    const parent = document.getElementById("table-container");
     parent.innerHTML = `<div class="container" id="wordList"></div>`;
-    const container = document.getElementById('wordList');
+    const container = document.getElementById("wordList");
 
     container.innerHTML = `
         <div class="row header">
@@ -226,89 +252,107 @@ const createTable = (data) => {
         </div>`;
 
     for (const word of data) {
-        const row = document.createElement('div');
-        row.className = 'row';
+        const row = document.createElement("div");
+        row.className = "row";
+        if (JSON.parse(localStorage.getItem(`${word.number}`))) {
+            row.style.backgroundColor = "#b3f5ff";
+        }
         if (JSON.parse(localStorage.getItem(`type-${word.number}`))) {
-            row.style.backgroundColor = '#cbffb3';
+            row.style.backgroundColor = "#cbffb3";
         }
 
-        const numberCell = document.createElement('div');
-        numberCell.className = 'cell number';
-        numberCell.textContent = word.number;
+        const numberCell = document.createElement("div");
+        numberCell.className = "cell number";
 
-        const wordCell = document.createElement('div');
-        wordCell.className = 'cell word';
+        const numberText = document.createElement("p");
+        numberText.className = "number-p number";
+        numberText.textContent = word.number;
+
+        const wordCell = document.createElement("div");
+        wordCell.className = "cell word";
         wordCell.textContent = word.word;
 
+        for (const miss of missList) {
+            if (miss.number === word.number) {
+                numberText.style.backgroundColor = "#f67171";
+                numberText.style.color = "#ffffff";
+                break;
+            }
+        }
+
+        numberCell.appendChild(numberText);
         row.appendChild(numberCell);
         row.appendChild(wordCell);
         container.appendChild(row);
     }
-}
+};
 
 const showPopUp = (number) => {
-    const wordBox = document.getElementById('pop-word');
-    const meanBox = document.getElementById('pop-mean');
+    const wordBox = document.getElementById("pop-word");
+    const meanBox = document.getElementById("pop-mean");
     wordBox.textContent = WORDS[number].word;
     meanBox.textContent = WORDS[number].mean;
-    document.querySelector('.pop-overlay').style.display = 'block';
-    document.querySelector('.pop-up').style.display = 'block';
-}
+    document.querySelector(".pop-overlay").style.display = "block";
+    document.querySelector(".pop-up").style.display = "block";
+};
 
 const closePopUp = () => {
-    document.querySelector('.pop-overlay').style.display = 'none';
-    document.querySelector('.pop-up').style.display = 'none';
-}
+    document.querySelector(".pop-overlay").style.display = "none";
+    document.querySelector(".pop-up").style.display = "none";
+};
 
 const wordTouchEvent = () => {
     let startY = 0;
     let moved = false;
     const threshold = 10;
 
-    const container = document.getElementById('wordList');
+    const container = document.getElementById("wordList");
 
-    container.addEventListener('touchstart', (e) => {
+    container.addEventListener("touchstart", (e) => {
         startY = e.touches[0].clientY;
         moved = false;
     });
 
-    container.addEventListener('touchmove', (e) => {
+    container.addEventListener("touchmove", (e) => {
         const currentY = e.touches[0].clientY;
         if (Math.abs(currentY - startY) > threshold) {
             moved = true;
         }
     });
 
-    container.addEventListener('touchend', (e) => {
+    container.addEventListener("touchend", (e) => {
         if (moved) return;
         e.preventDefault();
 
         const target = e.target;
-        const parent = target.parentNode;
-        const style = getComputedStyle(parent);
+        let parent = target.parentNode;
+        let style = getComputedStyle(parent);
 
-        if (target.classList[1] === 'word' && target.textContent !== '単語') {
+        if (target.classList[1] === "word" && target.textContent !== "単語") {
             const number = Number(parent.children[0].textContent);
             showPopUp(number);
         }
 
-        if (target.classList[1] === 'number' && target.textContent !== 'No.') {
+        if (target.classList[1] === "number" && target.textContent !== "No.") {
+            if (e.target.classList[0] === "number-p") {
+                parent = parent.parentNode;
+                style = getComputedStyle(parent);
+            }
             const number = Number(target.textContent);
-            if (style.backgroundColor === 'rgb(255, 255, 255)' || style.backgroundColor === 'rgb(179, 245, 255)') {
-                parent.style.backgroundColor = '#cbffb3';
+            if (style.backgroundColor === "rgb(255, 255, 255)" || style.backgroundColor === "rgb(179, 245, 255)") {
+                parent.style.backgroundColor = "#cbffb3";
                 localStorage.setItem(`type-${number}`, true);
             } else {
                 localStorage.setItem(`type-${number}`, false);
                 if (JSON.parse(localStorage.getItem(`${number}`))) {
-                    parent.style.backgroundColor = '#b3f5ff';
+                    parent.style.backgroundColor = "#b3f5ff";
                 } else {
-                    parent.style.backgroundColor = '#fff';
+                    parent.style.backgroundColor = "#fff";
                 }
             }
         }
     });
-}
-
+};
 
 window.addEventListener("DOMContentLoaded", () => {
     const selector = document.getElementById("range-select");
@@ -370,7 +414,6 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-
 const backBtnClick = () => {
     location.reload();
-}
+};
